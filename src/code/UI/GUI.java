5ac4f7ec4a2f5af806e4ea294feb452a6d2f8832
Oskar <rakso@bunbury.de>
2025-07-25 20:17:43 +0200

    first window

--- src/code/UI/GUI.java
+++ src/code/UI/GUI.java
@@ -1,5 +1,19 @@
 package code.UI;
 
-public class GUI {
-    
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI() {
+        setTitle("Flappy Bird");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        
+        Color lightBlue = new Color(135, 206, 250);
+        getContentPane().setBackground(lightBlue);
+        setIconImage(new ImageIcon(getClass().getResource("/ressources/images/Flappy Bird Icon.png")).getImage());
+    }
 }
--- src/code/main/Game.java
+++ src/code/main/Game.java
@@ -1,10 +1,17 @@
 package code.main;
 
-public class Game {
-    public Game(){
+import code.UI.GUI;
+import javax.swing.SwingUtilities;
 
+public class Game {
+    private final GUI gui;
+
+    public Game(){
+        gui = new GUI();
     }
     public void start(){
-        
+        SwingUtilities.invokeLater(() -> {
+            gui.setVisible(true); // Show the GUI
+        });
     }
 }
